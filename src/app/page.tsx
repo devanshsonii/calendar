66cf826c9fe4d74acc3227bf5f401831b61d89b8
devanshsonii <devanshsoni874@gmail.com>
2026-04-08 23:07:15 +0530

    deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import WallCalendar from "@/components/WallCalendar.tsx";
+import WallCalendar from "@/components/WallCalendar";
 export default function Home() {
   return (
     <main className="flex-1 flex items-center justify-center min-h-screen bg-background">
